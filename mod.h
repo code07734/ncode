7f0f1453066610b650ca0c6c88fe18f91c32c58b
Vladimir <47279914+code07734@users.noreply.github.com>
2022-05-14 07:14:49 +0700

    Update mod.h

--- mod.h
+++ mod.h
@@ -1,22 +1,45 @@
 //#include <intrin.h>
 //#include <immintrin.h>
-void mod(uint32_t *x, uint32_t div){
-	uint32_t bita,n=*x,c=1,mask,mod,num,coeff;
-	_BitScanReverse(&bita,div);
-	//printf("n=%d\n div=%d\n bita=%d\n", n,div,bita);
+void mod(uint64_t *x, uint64_t div){
+	uint64_t hbit,xbit,n=*x,c=1,mask,rem,num,coeff;
+	_BitScanReverse(&hbit,div);
+	hbit+=1;
+	mask=(1<<hbit)-1;
+	coeff=mask+1-div;
 	if(n<(2*div)){c=0;}
 	while(c!=0){
-		mask=(1<<(bita+1))-1;
-		mod=n&mask;
-		num=n>>(bita+1);
-		coeff=(1<<(bita+1))-div;
-		n=(coeff*num)+mod;
-		//printf("mask=%d\n mod=%d\n num=%d\n coeff=%d\n n=%d\n\n", mask,mod,num,coeff,n);
-		//printf("%d\n", n);
+		//_BitScanReverse(&xbit,n);
+		//n-=(div<<(xbit-hbit));
+		rem=n&mask;
+		num=n>>hbit;
+		n=(coeff*num)+rem;
+		if(2*div<n && n<(3*div)){c=0;n-=div;}
 		if(n<(2*div)){c=0;}
+		printf("%lld\n", n);
 	}
 	if(n>div){n-=div;}
 	if(n==div){n=0;}
 	
 	*x=n;
+}
+
+void qmod(uint64_t *x, uint64_t div){
+	uint64_t hbit,n=*x,c=1,mask,rem,num,coeff;
+	_BitScanReverse(&hbit,div);
+	hbit+=1;
+	mask=(1<<hbit)-1;
+	rem=n&mask;
+	num=(1<<hbit)-div;
+	n>>=hbit;
+	while(c!=0){
+		rem+=num&(0-(n&1));
+		n>>=1;
+		num<<=1;
+		if(num>=div){num-=div;}
+		if(n==0 && rem>=div){n=rem;rem=n&mask;n>>=hbit;num=(1<<hbit)-div;}
+		if(n==0){c=0;}
+		//printf("%lld %lld\n", n, rem);
+	}
+	if(rem>=div){rem-=div;}
+	*x=rem;
 }
