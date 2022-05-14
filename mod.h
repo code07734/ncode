0aed4c20cf46db6c1dd9e6ff007915b71c9028d3
Vladimir <47279914+code07734@users.noreply.github.com>
2022-05-14 12:01:36 +0700

    Update mod.h

--- mod.h
+++ mod.h
@@ -1,21 +1,18 @@
 //#include <intrin.h>
 //#include <immintrin.h>
 void mod(uint64_t *x, uint64_t div){
-	uint64_t hbit,xbit,n=*x,c=1,mask,rem,num,coeff;
+	uint64_t hbit,n=*x,c=1,mask,rem,num,coeff;
 	_BitScanReverse(&hbit,div);
 	hbit+=1;
 	mask=(1<<hbit)-1;
 	coeff=mask+1-div;
 	if(n<(2*div)){c=0;}
 	while(c!=0){
-		//_BitScanReverse(&xbit,n);
-		//n-=(div<<(xbit-hbit));
 		rem=n&mask;
 		num=n>>hbit;
 		n=(coeff*num)+rem;
-		if(2*div<n && n<(3*div)){c=0;n-=div;}
 		if(n<(2*div)){c=0;}
-		printf("%lld\n", n);
+		//printf("%lld\n", n);
 	}
 	if(n>div){n-=div;}
 	if(n==div){n=0;}
@@ -37,7 +34,7 @@ void qmod(uint64_t *x, uint64_t div){
 		num<<=1;
 		if(num>=div){num-=div;}
 		if(n==0 && rem>=div){n=rem;rem=n&mask;n>>=hbit;num=(1<<hbit)-div;}
-		if(n==0){c=0;}
+		c=n;
 		//printf("%lld %lld\n", n, rem);
 	}
 	if(rem>=div){rem-=div;}
